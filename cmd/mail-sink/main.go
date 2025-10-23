75d93d07a271d486913e6e1c04066fdde6d491d3
Filipe Zarco Lopes de Coutinho <filipezarcocoutinho@gmail.com>
2025-10-23 03:11:39 +0100

    refactor: use diff logger

--- cmd/mail-sink/main.go
+++ cmd/mail-sink/main.go
@@ -26,7 +26,7 @@ func main() {
 	// Load env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		logger.Log.Warn("Error loading .env file")
 	}
 
 	logger.InitLogger()
